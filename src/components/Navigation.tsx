d155ecde7f1bf121d1ad71e7f4882401d160a416
Muzammil-Ahm3d <mdmuzammilahmed254@gmail.com>
2026-01-17 21:23:06 +0530

    Use user provided logo directly

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -57,7 +57,7 @@ const Navigation = () => {
             className="flex items-center gap-3"
           >
             <img
-              src="/logo-v3.png"
+              src="/logo-final.png"
               alt="Belmond Holidays Logo"
               className="h-12 w-auto object-contain"
             />
