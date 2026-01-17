17a81f9e148aac6f5d7400cdf245eb511e0db256
Muzammil-Ahm3d <mdmuzammilahmed254@gmail.com>
2026-01-17 21:13:17 +0530

    Fix logo background - use logo-v2 with cache busting

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -57,7 +57,7 @@ const Navigation = () => {
             className="flex items-center gap-3"
           >
             <img
-              src="/logo.png"
+              src="/logo-v2.png"
               alt="Belmond Holidays Logo"
               className="h-12 w-auto object-contain"
             />
