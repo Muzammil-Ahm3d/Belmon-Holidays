380c4538b5cc6517f612e17504bf2004ee04c20b
Muzammil-Ahm3d <mdmuzammilahmed254@gmail.com>
2026-01-17 23:48:45 +0530

    Increase footer logo size again to h-40

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
               <img
                 src="/footer-logo.png"
                 alt="Belmond Holidays"
-                className="h-28 w-auto object-contain mix-blend-screen"
+                className="h-40 w-auto object-contain mix-blend-screen"
               />
             </a>
             <p className="text-card/70 mb-6 leading-relaxed">
